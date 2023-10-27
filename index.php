aa27972a273d6cd14516c8674f30ccb000b3ca97
Alex Rodrigues <alexsantosro@hotmail.com>
2023-10-27 13:12:50 -0300

    blog work

--- index.php
+++ index.php
@@ -17,22 +17,18 @@
 
     <div class="post-item">
 
-      <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
       <div class="metabox">
-        <p>Posted By Alex on 27/10/2023 in News</p>
+        <p>Posted By <?php the_author_posts_link() ?> on <?php the_time('j/n/Y') ?> in <?php echo get_the_category_list(', ') ?></p>
       </div>
       <div class="generic-content">
         <?php the_excerpt() ?>
-
+        <p><a class="btn btn--blue" href="<?php the_permalink() ?>">Continue Reading &raquo;</a></p>
       </div>
-
-
     </div>
-
   <?php
   }
-
+  echo paginate_links()
   ?>
 </div>
-
 <?php get_footer(); ?>
